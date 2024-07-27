13892e6310e276b4cab0e8567cfcb1dde595e2b0
Behran Kankul <behran.kankul@mackolik.com>
2024-07-27 17:58:30 +0300

    UI update

--- CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
+++ CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08731da14c22d4a5ab878a64774a8b45cd4ba887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a905df76515c53f6e298c93365e7f9d048b8133")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
