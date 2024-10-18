673addc0230752062f39d2e66de1cce4ebcd1f21
Behran Kankul <behrankankul@firmasoft.com>
2024-10-18 20:24:38 +0300

    Files added

--- CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
+++ CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2efec048e49d3eb0debe8e31389ade0554e8ecc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edf92623964dc2a7cdbcc8c21776cc4634238f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
