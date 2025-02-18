a658ec219e68c93831c5805785a71910b7453c2b
Behran Kankul <behrankankul@firmasoft.com>
2025-02-18 10:50:20 +0300

    New symbol Images added.

--- CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
+++ CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6894f476d03b8ca14d781d99f78cbc073ca16137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2bc4b139e57d3458f8ffaa4e4fbcfdd5bf3d05")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
