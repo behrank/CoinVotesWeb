f2044d5b579e388ee27f45a9ba97958ab03b9bfc
Behran Kankul <behrankankul@firmasoft.com>
2025-04-02 23:32:22 +0300

    new icons

--- CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
+++ CoinVotesWeb/CoinVotesWeb/obj/Debug/net8.0/CoinVotesWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a658ec219e68c93831c5805785a71910b7453c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8e013e87d6f52bebc2a8ce92e56868e924897c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoinVotesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
