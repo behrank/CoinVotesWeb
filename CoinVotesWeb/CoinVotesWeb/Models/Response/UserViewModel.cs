9ccb05113df746a63c2aad66079889f14de64456
Behran Kankul <behrank@gmail.com>
2025-05-23 12:15:50 +0300

    old files removed

--- CoinVotesWeb/CoinVotesWeb/Models/Response/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoinVotesWeb.Models.Response;
-
-public class UserViewModel
-{
-    public int Id { get; set; }
-    public string Email { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public string Device { get; set; }
-}
